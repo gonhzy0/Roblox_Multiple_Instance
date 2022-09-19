7d5e516f53f24e076b1e8e426589e4b32b9ec20e
twth <69372054+twth@users.noreply.github.com>
2022-09-19 21:38:06 +0100

    Update Rbx_multiple_instances.cpp

--- Rbx_multiple_instances.cpp
+++ Rbx_multiple_instances.cpp
@@ -5,110 +5,108 @@
 
 HANDLE FindHandle(DWORD pid)
 {
-    HANDLE Process = OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
+ HANDLE Process = OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
 
-    SYSTEM_HANDLE_INFORMATION handleInfo;
+ SYSTEM_HANDLE_INFORMATION handleInfo;
 
-    PSYSTEM_HANDLE_INFORMATION pSysHandleInfo = new SYSTEM_HANDLE_INFORMATION;
-    ULONG retlen;
-    DWORD size = sizeof(SYSTEM_HANDLE_INFORMATION);
-    NTSTATUS status = NtQuerySystemInformation(16,
-        pSysHandleInfo, size, &retlen);
+ PSYSTEM_HANDLE_INFORMATION pSysHandleInfo = new SYSTEM_HANDLE_INFORMATION;
+ ULONG retlen;
+ DWORD size = sizeof(SYSTEM_HANDLE_INFORMATION);
+ NTSTATUS status = NtQuerySystemInformation(16,
+ pSysHandleInfo, size, &retlen);
 
-    delete pSysHandleInfo;
-    size = retlen + 1024;
+ delete pSysHandleInfo;
+ size = retlen + 1024;
 
-    PSYSTEM_HANDLE_INFORMATION HandleInfo = (PSYSTEM_HANDLE_INFORMATION)new BYTE[size];
+ PSYSTEM_HANDLE_INFORMATION HandleInfo = (PSYSTEM_HANDLE_INFORMATION)new BYTE[size];
 
-    NtQuerySystemInformation(16,
-        HandleInfo, size, &retlen);
+ NtQuerySystemInformation(16,
+ HandleInfo, size, &retlen);
 
-    for (ULONG i = 0; i < HandleInfo->NumberOfHandles; i++)
-    {
+ for (ULONG i = 0; i < HandleInfo->NumberOfHandles; i++)
+ {
 
-        SYSTEM_HANDLE pHandle = (HandleInfo->Handles[i]);
+ SYSTEM_HANDLE pHandle = (HandleInfo->Handles[i]);
 
-        if (pHandle.ProcessId == pid)
-        {
+ if (pHandle.ProcessId == pid)
+ {
+ HANDLE DuplicatedHandle;
 
+ BOOL b = DuplicateHandle(Process, (HANDLE)pHandle.Handle,
+ GetCurrentProcess(), &DuplicatedHandle, 0, FALSE, DUPLICATE_SAME_ACCESS);
+ if (!b) continue;
 
-            HANDLE DuplicatedHandle;
+ PVOID nameBuffer = malloc(0x1000);
 
-            BOOL b = DuplicateHandle(Process, (HANDLE)pHandle.Handle,
-                GetCurrentProcess(), &DuplicatedHandle, 0, FALSE, DUPLICATE_SAME_ACCESS);
-            if (!b) continue;
+ if (!NT_SUCCESS(NtQueryObject((HANDLE)DuplicatedHandle, ObjectNameInformation,
+ nameBuffer, 0x1000, nullptr))) continue;
 
-            PVOID nameBuffer = malloc(0x1000);
-
-            if (!NT_SUCCESS(NtQueryObject((HANDLE)DuplicatedHandle, ObjectNameInformation,
-                nameBuffer, 0x1000, nullptr))) continue;
-
-            auto name = reinterpret_cast<UNICODE_STRING*>(nameBuffer);
+ auto name = reinterpret_cast<UNICODE_STRING*>(nameBuffer);
 
 
-            if (name->Length > 29 && wcscmp(name->Buffer + 29, L"ROBLOX_singletonEvent") == 0)
-            {
-                printf("Handle Found and Closing:  %S \n You can now open another roblox instance! ", name->Buffer);
-                DuplicateHandle(Process, (HANDLE)pHandle.Handle, ::GetCurrentProcess(), &DuplicatedHandle,
-                    0, FALSE, DUPLICATE_CLOSE_SOURCE);
+ if (name->Length > 29 && wcscmp(name->Buffer + 29, L"ROBLOX_singletonEvent") == 0)
+ {
+ printf("Handle Found and Closing: %S \n You can now open another roblox instance! ", name->Buffer);
+ DuplicateHandle(Process, (HANDLE)pHandle.Handle, ::GetCurrentProcess(), &DuplicatedHandle,
+ 0, FALSE, DUPLICATE_CLOSE_SOURCE);
 
-                free(nameBuffer);
+ free(nameBuffer);
 
-                CloseHandle(Process);
+ CloseHandle(Process);
 
-                return DuplicatedHandle;
+ return DuplicatedHandle;
 
-            }
+ }
 
 
 
-        }
+ }
 
-    }
+ }
 
-    printf("Handle Already Closed.");
-    CloseHandle(Process);
-    return 0;
+ printf("Handle Already Closed.");
+ CloseHandle(Process);
+ return 0;
 }
 
 void CloseHandle() {
-    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (hSnapshot == INVALID_HANDLE_VALUE)
-        return;
+ HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+ if (hSnapshot == INVALID_HANDLE_VALUE)
+ return;
 
-    PROCESSENTRY32 pe;
-    pe.dwSize = sizeof(pe);
+ PROCESSENTRY32 pe;
+ pe.dwSize = sizeof(pe);
 
 
-    Process32First(hSnapshot, &pe);
+ Process32First(hSnapshot, &pe);
 
-    DWORD pid = 0;
-    while (Process32Next(hSnapshot, &pe)) {
-        if (_wcsicmp(pe.szExeFile, L"RobloxPlayerBeta.exe") == 0) {
-            CloseHandle(hSnapshot);
+ DWORD pid = 0;
+ while (Process32Next(hSnapshot, &pe)) {
+ if (_wcsicmp(pe.szExeFile, L"RobloxPlayerBeta.exe") == 0) {
+ CloseHandle(hSnapshot);
 
-            pid = pe.th32ProcessID;
+ pid = pe.th32ProcessID;
 
-            HANDLE handle = FindHandle(pid);
+ HANDLE handle = FindHandle(pid);
 
-            CloseHandle(handle);
+ CloseHandle(handle);
 
-            return;
-        }
+ return;
+ }
 
-    }
-    std::cout << "Make Sure Roblox is running!" << std::endl;
+ }
+ std::cout << "Make Sure Roblox is running!" << std::endl;
 
 }
 
 int main()
 {
 
-    NtQuerySystemInformation = (fnNtQuerySystemInformation)GetProcAddress(GetModuleHandleA("ntdll"), "NtQuerySystemInformation");
-    NtQueryObject = (fnNtQueryObj)GetProcAddress(GetModuleHandleA("ntdll"), "NtQueryObject");
+ NtQuerySystemInformation = (fnNtQuerySystemInformation)GetProcAddress(GetModuleHandleA("ntdll"), "NtQuerySystemInformation");
+ NtQueryObject = (fnNtQueryObj)GetProcAddress(GetModuleHandleA("ntdll"), "NtQueryObject");
 
-    CloseHandle();
+ CloseHandle();
 
-    std::cin.get();
+ std::cin.get();
 }
 
